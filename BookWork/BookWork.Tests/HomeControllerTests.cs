923ee879cff7a7c62d2a447511882ec7add44fcf
unknown <vark47@aol.com>
2018-02-17 21:32:52 -0800

    first commit

---
+++ BookWork/BookWork.Tests/HomeControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WorkingWithVisualStudio.Controllers;
+using WorkingWithVisualStudio.Models;
+using Xunit;
+namespace WorkingWithVisualStudio.Tests
+{
+    public class HomeControllerTests
+    {
+        class ModelCompleteFakeRepository : IRepository
+        {
+            public IEnumerable<Product> Products { get; set; }
+            public void AddProduct(Product p)
+            {
+                // do nothing - not required for test
+            }
+        }
+        [Theory]
+        [ClassData(typeof(ProductTestData))]
+        public void IndexActionModelIsComplete(Product[] products)
+        {
+            // Arrange
+            var controller = new HomeController();
+            controller.Repository = new ModelCompleteFakeRepository
+            {
+                Products = products
+            };
+            // Act
+            var model = (controller.Index() as ViewResult)?.ViewData.Model
+            as IEnumerable<Product>;
+            // Assert
+            Assert.Equal(controller.Repository.Products, model,
+            Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name
+            && p1.Price == p2.Price));
+        }
+    }
+}
